9326300817351801d42fc74e03e373496c31c722
SHIV PRAKASH SINGH <sshivprakash826@gmail.com>
2025-10-19 11:26:50 +0530

    Add gear property and methods to Car class
    Added gear property with getter and setter methods.

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -5,6 +5,15 @@ package com.driver;
 public class Car extends Vehicle {
     private String name;
     private boolean isManual;
+    private int gear;
+
+public int getGear() {
+    return gear;
+}
+
+public void changeGear(int gear) {
+    this.gear = gear;
+}
 
     public Car(String name, boolean isManual) {
         this.name = name;
