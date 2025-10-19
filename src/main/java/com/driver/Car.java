dc1a4e0a6d5080dfd0b95ec77a9dbf75f7495e92
SHIV PRAKASH SINGH <sshivprakash826@gmail.com>
2025-10-19 11:42:41 +0530

    Update Car.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -1,30 +1,35 @@
 package com.driver;
 
 public class Car extends Vehicle {
-    private String name;
+    private int wheels;
+    private String type;
+    private int doors;
+    private int gears;
     private boolean isManual;
-    private int gear;
+    private int currentGear;
+    private int seats;
 
-    public Car(String name, boolean isManual) {
-        super(name);
-        this.name = name;
+    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
+        super(name); // Call Vehicle constructor
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
         this.isManual = isManual;
-        this.gear = 0; // Initialize gear
+        this.type = type;
+        this.seats = seats;
+        this.currentGear = 0; // Initialize gear
     }
 
-    public boolean isManual() {
-        return isManual;
+    public void changeGear(int newGear) {
+        this.currentGear = newGear;
+        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+    }
+
+    public void changeSpeed(int newSpeed, int newDirection) {
+        System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 
     public int getGear() {
-        return gear;
-    }
-
-    public void changeGear(int gear) {
-        this.gear = gear;
-    }
-
-    public void drive() {
-        System.out.println("Car is driving");
+        return currentGear;
     }
 }
