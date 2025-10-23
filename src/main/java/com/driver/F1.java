9318d41d2a2c7c38b266dcbc773170b1c4832ad5
SHIV PRAKASH SINGH <sshivprakash826@gmail.com>
2025-10-23 20:09:39 +0530

    Update F1.java

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -1,22 +1,31 @@
 package com.driver;
 
 public class F1 extends Car {
-    private int speed;
-    private int direction;
 
     public F1(String name, boolean isManual) {
-        // Arbitrary values for wheels, doors, gears, type, seats
-        super(name, 4, 2, 6, isManual, "F1", 1);
-        this.speed = 0;
-        this.direction = 0;
+        //Use arbitrary values for parameters which are not mentioned
+        super(name, 4, 4,6, isManual, "racing", 1);
     }
 
-    public void accelerate(int rate) {
-        int newSpeed = this.speed + rate;
+    public void accelerate(int rate){
+        int newSpeed = getCurrentSpeed() + rate; //set the value of new speed by using currentSpeed and rate
+        /**
+         * speed 0: gear 1
+         * speed 1-50: gear 1
+         * speed 51-100: gear 2
+         * speed 101-150: gear 3
+         * speed 151-200: gear 4
+         * speed 201-250: gear 5
+         * speed more than 250: gear 6
+         */
 
-        if (newSpeed == 0) {
+        if(newSpeed == 0) {
+            //Stop the car, set gear as 1
+            stop();
             changeGear(1);
-        } else if (newSpeed <= 50) {
+        }
+        //for all other cases, change the gear accordingly
+        else if (newSpeed <= 50){
             changeGear(1);
         } else if (newSpeed <= 100) {
             changeGear(2);
@@ -26,49 +35,12 @@ public class F1 extends Car {
             changeGear(4);
         } else if (newSpeed <= 250) {
             changeGear(5);
-        } else {
+        }
+        else {
             changeGear(6);
         }
-
-        this.speed = newSpeed;
-
-        if (newSpeed > 0) {
+        if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
-        } else {
-            // ensure the vehicle state is updated when speed becomes zero
-            changeSpeed(0, getCurrentDirection());
         }
-    }
-
-    public int getCurrentSpeed() {
-        return this.speed;
-    }
-
-    @Override
-    public int getCurrentDirection() {
-        return this.direction;
-    }
-
-    public void changeSpeed(int speed, int direction) {
-        this.speed = speed;
-        this.direction = direction;
-        // keep Vehicle's internal state in sync
-        super.move(speed, direction);
-        System.out.println("changeSpeed method called - The speed is changed to: " + speed + ", and the direction is changed to: " + direction + " degrees");
-    }
-
-    public void changeGear(int gear) {
-        super.changeGear(gear);
-    }
-
-    public int getCurrentGear() {
-        return super.getGear();
-    }
-
-    @Override
-    public void steer(int angle) {
-        // update local direction and propagate to parent
-        this.direction += angle;
-        super.steer(angle);
     }
 }
